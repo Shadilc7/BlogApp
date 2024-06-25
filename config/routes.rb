2576265a460670e03840746b20a7eaef56a05405
shadil <shadilc7@gmail.com>
2024-06-25 22:48:29 +0530

    adds cover image for blog post

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   # post 'blog_posts', to: "blog_posts#create", as: :blog_posts
 
 
-resources :blog_posts
+resources :blog_posts do
+  resource :cover_image, only: [:destroy], module: :blog_posts 
+end
 
   # Defines the root path route ("/")
   root "blog_posts#index"
